2af10a4ecbfb71b9147336aa8bc3e6f7ff9e0024
stellasoftware <36570677+stellasoftware@users.noreply.github.com>
2018-03-04 11:11:10 -0500

    decompiled reference.list.city.ts

---
+++ reference/reference.list.city.ts
@@ -0,0 +1,27 @@
+ import { Reference } from 'reference/reference';
+ 
+ import { ReferenceInterface } from 'reference/reference.interface';
+ 
+ 
+ class ReferenceFunctionSort extends Reference
+ {
+ 
+    constructor(a: object, b: object, c: (a: string, b: any) => any)
+    {
+        super(a);
+    }
+    
+    public select(a: string, b: object) : object
+    {
+        return [ 'Oslo', 'Stockholm', 'Helsinki', 'Berlin', 'Rome', 'Madrid' ];
+    }
+    
+    public update(a: string, b: object) : object
+    {
+        this.reference = b;
+    }
+ 
+ }
+ 
+ 
+ export { ReferenceFunctionSort };
